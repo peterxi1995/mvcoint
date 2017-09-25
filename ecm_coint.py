fixed bug in calculation

ecm_coint.py
@@ -41,7 +41,7 @@ class VAR:
         self.data = data
 
         num_of_cointegration = len(rejected_r_values)
-        self.um_of_cointegration = num_of_cointegration
+        self.num_of_cointegration = num_of_cointegration
 
         if num_of_cointegration==0:
             # Suppose there is no cointegration relationship, reduce the model to general log-normal model with low statistical power and premium
@@ -249,6 +249,33 @@ class ECMCoint:
         print ("Implied Sharpe: %s"%self.price_of_risk)
         print ("Arbitrage Index: %s"%self.arbitrage_index)
 
+    def get_coefficients(self,maturity,X0,Y_hat):
+        # Get the coefficients needed to calculate the optimal allocation
+        # This should be called at time 0
+        cov = self.sigma.dot(self.sigma.T)
+        cov_inv = inv(cov)
+        NUM_OF_ASSETS = len(self.A)
+
+        D = np.diag(np.diag(cov))
+
+        THETA = 0.5*self.A.dot(D).dot(np.ones([NUM_OF_ASSETS,1])) - self.r*self.A.dot(np.ones([NUM_OF_ASSETS,1]))
+        alpha = self.theta - self.A.dot(X0) + 0.5*D.dot(np.ones([NUM_OF_ASSETS,1])) - self.r*np.ones([NUM_OF_ASSETS,1])
+
+        # aim is to get the lambda
+        def K_hat(t,T):
+            return 2*cov_inv*(T-t)
+        def N_hat(t,T):
+            return (THETA.T.dot(cov_inv)*(T-t)**2).T
+        def M_hat(t,T):
+            return (1/3)*float(THETA.T.dot(cov_inv).dot(THETA))*(T-t)**3 + np.trace(self.sigma.T.dot(self.A.T).dot(cov_inv).dot(self.A).dot(self.sigma))/2 *(T-t)**2
+        def H_hat(T):
+            return 0.5*alpha.T.dot(K_hat(0,maturity)).dot(alpha) + N_hat(0,maturity).T.dot(alpha)+M_hat(0,maturity)
+
+        H_hat_T = float(H_hat(maturity))
+
+        self.lambda = (Y_hat - np.exp(self.r*maturity)*1)/H_hat_T
+
+
     def solve_time_consistent(self,t,maturity,X_t,Y_0,Y_hat):
         '''
         give out a solution that is time-consistent
@@ -256,6 +283,8 @@ class ECMCoint:
         
         The optimal control u only concerns the current price level, time left, initial wealth and target;
         it does not care about the current state of wealth
+
+        Must be done after self.get_coefficients have been called
         '''
         cov = self.sigma.dot(self.sigma.T)
         cov_inv = inv(cov)
@@ -271,22 +300,13 @@ class ECMCoint:
             return 2*cov_inv*(T-t)
         def N_hat(t,T):
             return (THETA.T.dot(cov_inv)*(T-t)**2).T
-        def M_hat(t,T):
-            return (1/3)*float(THETA.T.dot(cov_inv).dot(THETA))*(T-t)**3 + np.trace(self.sigma.T.dot(self.A.T).dot(cov_inv).dot(self.A).dot(self.sigma))/2 *(T-t)**2
-
 
         alpha = self.theta - self.A.dot(X_t) + 0.5*D.dot(np.ones([NUM_OF_ASSETS,1])) - self.r*np.ones([NUM_OF_ASSETS,1])
 
         # H_hat is only needed for (H_hat(0,alpha(0),T)):
 
-        def H_hat(T):
-            return 0.5*alpha.T.dot(K_hat(0,maturity)).dot(alpha) + N_hat(0,maturity).T.dot(alpha)+M_hat(0,maturity)
-
-        H_hat_T = float(H_hat(T))
-
-
         # The Y_0 should not have been changed
-        u = (Y_hat*np.exp(-self.r*(maturity-t))-Y_0*np.exp(self.r*t))/(H_hat_T) * ((cov_inv+self.A.T.dot(K_hat(t,maturity))).dot(alpha)+self.A.T.dot(N_hat(t,maturity)))
+        u = self.lambda * np.exp(self.r*(maturity-t)) * ((cov_inv+self.A.T.dot(K_hat(t,maturity))).dot(alpha)+self.A.T.dot(N_hat(t,maturity)))
 
         return u
 

tester_one_pair.py
@@ -55,7 +55,9 @@ class Strategy:
         for i in range(num_of_assets):
             X0[i,0] = self.assetHistClose[var_model.assetNames[i]][-1]
         
-        weight = var_model.solve(self.maturity-self.timeLeft,self.maturity,X0,1,1.1).flatten()
+        weight = var_model.solve(self.maturity-self.timeLeft,self.maturity,X0,1,1.01).flatten()
+        print weight
+        raw_input(3)
         #if self.current_asset<=1.01:
         #    weight = var_model.solve(T,X0,self.current_asset,1.01).flatten()
         #elif self.current_asset>1.01:
@@ -130,7 +132,7 @@ def calculate_pnl(signal_df,data,assetNames):
 
 
 def main():
-    data = pd.read_csv('2800_2828.csv',index_col=0,parse_dates=True)
+    data = pd.read_csv('SPY_IVV.csv',index_col=0,parse_dates=True)
     data = np.log(data)
     signal_list = []
 
@@ -152,8 +154,6 @@ def main():
         #if strategy.timeLeft<strategy.maturity-1:
         #    signal = last_signal
         last_signal = signal
-        print signal
-        raw_input(3)
         signal_list.append(signal)
 
     signal_df = pd.DataFrame(signal_list)